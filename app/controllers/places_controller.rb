853e361bba6f28fffa14140f23821ce71d6f03e3
Beth <bether1@gmail.com>
2019-01-31 21:54:21 +0000

    adding the map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,13 +2,12 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
-    @places = Place.paginate(:page => params[:page]).per_page(3)
+    @places = Place.paginate(:page => params[:page]).per_page(5)
   end
 
   def new
     @place = Place.new
   end
-
 
   def create
   @place = current_user.places.create(place_params)
@@ -33,11 +32,9 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
-
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-
     @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
