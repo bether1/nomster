4dcc41eae7c2ee79b767a21f786e72abd18c5cf4
Beth <bether1@gmail.com>
2019-02-13 19:31:28 +0000

    bug fixing3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :places
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :photos
 end
